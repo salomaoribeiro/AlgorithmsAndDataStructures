da6876e19742b983c5fe0415b9d4aa07432475d5
Salomão Ribeiro Pires <salomaomdm@gmail.com>
2024-05-12 15:09:23 -0300

    reverseint - using math to get always the last number

--- AlgoCasts/exercises/03-reverseint/index.js
+++ AlgoCasts/exercises/03-reverseint/index.js
@@ -9,19 +9,30 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-  let value = "0";
+  // let value = "0";
+  //
+  // if (n < 0) {
+  //   value = "-";
+  //   n *= -1;
+  // }
+  //
+  // while (n != 0) {
+  //   value += n % 10;
+  //   n = parseInt(n / 10);
+  // }
+  //
+  // return parseInt(value);
 
-  if (n < 0) {
-    value = "-";
-    n *= -1;
-  }
+  let value = 0;
+  const iteration = n < 0 ? n.toString().length - 1 : n.toString().length;
 
-  while (n != 0) {
-    value += n % 10;
+  for (let i = iteration - 1; i >= 0; i--) {
+    const number = n % 10;
     n = parseInt(n / 10);
+    value += 10 ** i * number;
   }
 
-  return parseInt(value);
+  return value;
 }
 
 module.exports = reverseInt;
