0d2f0f3df3cadb978e87e943fbac51ed2944f5e4
Salomão Ribeiro Pires <salomaomdm@gmail.com>
2024-05-17 07:12:52 -0300

    captalize - using array, toUpperCase, slice and join

--- AlgoCasts/exercises/08-capitalize/index.js
+++ AlgoCasts/exercises/08-capitalize/index.js
@@ -7,6 +7,14 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let result = [];
+
+  str.split(" ").forEach((element) => {
+    result.push(element[0].toUpperCase() + element.slice(1));
+  });
+
+  return result.join(" ");
+}
 
 module.exports = capitalize;
